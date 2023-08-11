9ba89b037899bca67979da50409add0ace92c673
abrowne2 <adamo.browne@gmail.com>
2023-08-11 16:43:46 -0400

    Durian: Initial process PID get

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use process_memory::{Memory, DataMember, TryIntoProcessHandle, Pid, ProcessHandl
 pub mod memory_utils;
 use memory_utils::{get_pid};
 
-fn main() {
-    let baldurs_gate_process = "Arc";
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let baldurs_gate_process = "bg3_dx11.exe";
 
     let baldur_pid: Pid = get_pid(baldurs_gate_process).unwrap();
     let handle = baldur_pid.try_into_process_handle().unwrap();
 
+    // let target_godmode_sequence 
+
     println!("Inside here");
+
+    Ok(())
 }
--- src/memory_utils/mod.rs
+++ src/memory_utils/mod.rs
@@ -1,18 +1,20 @@
 extern crate sysinfo;
-use process_memory::Pid;
+use process_memory::{Pid, TryIntoProcessHandle, ProcessHandle};
 
-use sysinfo::{ProcessExt, System, SystemExt};
+use sysinfo::{ProcessExt, System, SystemExt, AsU32};
 
-pub fn get_pid(process_name: &str) -> Option<i32> {
-    let mut system = System::new_all();
-    system.refresh_all();
+pub fn get_pid(process_name: &str) -> Option<Pid> {
+    let mut system = System::new();
+    system.refresh_processes();
 
     for (pid, process) in system.processes() {
-        let process_cmd = process.cmd();
-        for cmd in process_cmd {
+        let cmds = process.cmd();
+        for cmd in cmds {
             if cmd.contains(process_name) {
-                print!("Found process: {} with pid: {}", process_name, pid);
-                return Some(*pid);
+                println!("Found process: {} with pid: {}", process_name, pid);
+
+                let casted_process_pid = pid.as_u32() as Pid;
+                return Some(casted_process_pid);    
             }
         }
     }
@@ -20,27 +22,27 @@ pub fn get_pid(process_name: &str) -> Option<i32> {
     None
 }
 
-// pub fn array_of_bytes_scan(process_handle: &ProcessHandle, target_sequence: &[Option<u8>], start_address: usize, end_address: usize) -> Result<Option<usize>> {
-//     // Determine the chunk size for scanning (e.g., 1024 bytes)
-//     let chunk_size = 1024;
-//     let mut buffer = vec![0; chunk_size];
+pub fn array_of_bytes_scan(process_handle: &ProcessHandle, target_sequence: &[Option<u8>], start_address: usize, end_address: usize) -> Result<Option<usize>> {
+    // Determine the chunk size for scanning (e.g., 1024 bytes)
+    let chunk_size = 1024;
+    let mut buffer = vec![0; chunk_size];
 
-//     // Iterate through the memory range
-//     for address in (start_address..end_address).step_by(chunk_size) {
-//         // Read a chunk of memory into the buffer
-//         process_handle.copy_address(address, &mut buffer)?;
+    // Iterate through the memory range
+    for address in (start_address..end_address).step_by(chunk_size) {
+        // Read a chunk of memory into the buffer
+        process_handle.copy_address(address, &mut buffer)?;
 
-//         // Iterate through the buffer and compare to the target sequence
-//         for i in 0..(chunk_size - target_sequence.len()) {
-//             if target_sequence.iter().enumerate().all(|(j, &target_byte)| {
-//                 target_byte.map_or(true, |target_byte| target_byte == buffer[i + j])
-//             }) {
-//                 // Found the sequence, return the address
-//                 return Ok(Some(address + i));
-//             }
-//         }
-//     }
+        // Iterate through the buffer and compare to the target sequence
+        for i in 0..(chunk_size - target_sequence.len()) {
+            if target_sequence.iter().enumerate().all(|(j, &target_byte)| {
+                target_byte.map_or(true, |target_byte| target_byte == buffer[i + j])
+            }) {
+                // Found the sequence, return the address
+                return Ok(Some(address + i));
+            }
+        }
+    }
 
-//     // Sequence not found, return None
-//     Ok(None)
-// }
+    // Sequence not found, return None
+    Ok(None)
+}
